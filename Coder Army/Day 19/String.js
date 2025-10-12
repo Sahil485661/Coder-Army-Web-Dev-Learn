8d3ca6d872840135fff30cd61dd35ef34eb22b90
Sahil485661 <vishwakarmasahil762@gmail.comgit config --global user.email>
2025-10-13 00:15:28 +0530

    day 19

--- Coder Army/Day 19/String.js
+++ Coder Army/Day 19/String.js
@@ -11,4 +11,22 @@ const str5 = `Here we cut the letter by using slice in java script`;
 console.log(str5.slice(5,15));
 const str6 = str1 + str2;
 console.log(str6);
+const str7 ="Anil Sahil Rahul Anas Rohit Avi";
+console.log(str7.split(" "));
+const today = new Date();
+
+console.log(today.toString());
+console.log(today.getMilliseconds());
+// Here we can create our own date
+// Month me 0 to 11 hota hai means jan = 0, Dec = 11
+const now = new Date(2025,8,20,8,25,16,125);
+console.log(now.toString());
+
+// here we are find time stamp
+// const timestamp = Date.now();
+const timechange = new Date(-302395346345);
+console.log(timechange);
+
+
+
 
