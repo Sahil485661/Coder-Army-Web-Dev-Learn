8e85872d8f1bac6099ae0ff762e9949ab327d3c7
Sahil485661 <raoganpat762@gmail.com>
2026-06-27 13:58:37 +0530

    Day 39 Final JS complete Now only needed interview preparation

---
+++ Coder Army/Day 34/forloopExercise.js
@@ -0,0 +1,7 @@
+function count(){
+    for(i = 0; i<= 1e9; i++){
+        count++;
+        console.log(count);
+    }
+}
+count();
---
+++ Coder Army/Day 39/Array fill.js
@@ -0,0 +1,5 @@
+let a = [43,23,23,44,65,33,221];
+a.fill(55,2,4);  // fill( value, start array index, end array index) array me  value dalne ke liye beech me kahi bhi 
+console.log(a)
+a.fill(0,0,1);
+console.log(a)
